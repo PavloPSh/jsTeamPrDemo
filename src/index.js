2be49d8ecf3e30e850dc35e364dd69ae2918df12
Pavlo Psheborovskyi <pavlopshe@gmail.com>
2022-08-26 20:37:01 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './js/loader.js'
+import './js/apiServiсe.js';
 import './js/form.js';
 import './js/homeRendering.js';
-import './js/apiServiсe.js';
 import './js/scroll.js';
 import './js/modal-footer.js';
 import './js/popup-modal';
